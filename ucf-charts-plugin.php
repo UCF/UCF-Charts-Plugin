debba912548c1c8a31ed2814c9c2310a8743241c
Jo Dickson <jogreybill@gmail.com>
2021-01-13 16:57:18 -0500

    Upgraded chartjs to latest v1.x release

--- ucf-charts-plugin.php
+++ ucf-charts-plugin.php
@@ -18,7 +18,7 @@ define( 'UCF_CHARTS__PLUGIN_URL', plugins_url( basename( dirname( __FILE__ ) ) )
 define( 'UCF_CHARTS__STATIC_URL', UCF_CHARTS__PLUGIN_URL . '/static' );
 define( 'UCF_CHARTS__JS_URL', UCF_CHARTS__STATIC_URL . '/js' );
 
-define( 'UCF_CHARTS__VENDOR_JS_URL', 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.min.js' );
+define( 'UCF_CHARTS__VENDOR_JS_URL', 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.1.1/Chart.min.js' );
 
 include_once 'admin/ucf-charts-config.php';
 include_once 'admin/ucf-charts-admin.php';
